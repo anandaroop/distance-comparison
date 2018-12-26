c615922a13b887cd18d86ffca4ddb7bc6490cdd8
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-12-26 16:53:29 -0500

    Add a Distance class that knows both algos

---
+++ distance.js
@@ -0,0 +1,32 @@
+import * as turf from '@turf/turf'
+import GeographicLib from 'geographiclib'
+
+export class Distance {
+  static haversine(p1, p2) {
+    return new Distance(p1, p2).haversineDistance()
+  }
+
+  static karney(p1, p2) {
+    return new Distance(p1, p2).karneyDistance()
+  }
+
+  constructor(p1, p2) {
+    this.p1 = p1
+    this.p2 = p2
+  }
+
+  haversineDistance() {
+    const { p1, p2 } = this
+    const from = turf.point([p1.lng, p1.lat])
+    const to = turf.point([p2.lng, p2.lat])
+    const distance = turf.distance(from, to) // km
+    return (distance * 1000).toFixed(3) // meters
+  }
+
+  karneyDistance() {
+    const { p1, p2 } = this
+    const geodesic = GeographicLib.Geodesic.WGS84
+    const r = geodesic.Inverse(p1.lat, p1.lng, p2.lat, p2.lng)
+    return r.s12.toFixed(3) // meters
+  }
+}
